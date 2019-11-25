574df802288ea7c032c6734f6ae945ed87c34303
mescal <achung020@gmail.com>
2019-11-25 19:19:27 +0800

    user model and seed tested&added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.destroy_all
+
+
+users = [
+  { name:'Mo', description: 'Hello World', email: 'ichung010@gmail.com', phone_number: '12345678', open_id: 'achung020' },
+  { name:'Fabien', description: 'Hello World', email: 'ichung010@gmail.com', phone_number: '12345678', open_id: 'achung020' },
+  { name:'Brian', description: 'Hello World', email: 'ichung010@gmail.com', phone_number: '12345678', open_id: 'achung020'  },
+  { name:'Forrest', description: 'Hello World', email: 'ichung010@gmail.com', phone_number: '12345678', open_id: 'achung020'  }
+]
+
+User.create(users)
+
+puts User.all
